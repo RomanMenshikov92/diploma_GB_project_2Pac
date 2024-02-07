6bdc79e755bf7ef412b6351a1d562708f3f9336e
Roman Menshikov <R-Tupac@yandex.ru>
2024-02-07 14:03:14 +0300

    fix-4

---
+++ src/data/firebase/key.ts
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyArcypl9mgmUyAd3lgfaJGuwkOKHaddYt4",
+  authDomain: "files-datebase-2pac.firebaseapp.com",
+  projectId: "files-datebase-2pac",
+  storageBucket: "files-datebase-2pac.appspot.com",
+  messagingSenderId: "666329118470",
+  appId: "1:666329118470:web:13e98b4f64fcf1fbd1d087",
+  measurementId: "G-DZ7F85X960",
+};
+
+export default firebaseConfig;
